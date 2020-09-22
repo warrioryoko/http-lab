a59a524b4db32f2f9646e61d21a77b1ea4294138
warrioryoko <miangpellegri@gmail.com>
2020-09-21 18:24:50 -0700

    Red route working. Echo route is not, don't know why.

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -8,4 +8,21 @@ describe('routes', () => {
 
     expect(response.text).toEqual('DERP');
   });
+
+  it('responds with status code 200 and the request body on /echo with POST', async() => {
+    const response = await request(app)
+      .post('/echo')
+      .send({ body: 'Herro pwease' });
+
+    expect(response.text)
+      .toEqual('Herro pwease');
+  });
+
+  it('responds with an h1 and the text red on /red with GET', async() => {
+    const response = await request(app)
+      .get('/red');
+
+    expect(response.text)
+      .toEqual('<h1>red</h1>');
+  });
 });
