5c0686e82b9e2ad9e0adefd9b68d4850da979719
warrioryoko <miangpellegri@gmail.com>
2020-09-21 18:30:57 -0700

    Green and Blue routes with tests working. They're the same as Red

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -25,4 +25,20 @@ describe('routes', () => {
     expect(response.text)
       .toEqual('<h1>red</h1>');
   });
+
+  it('responds with an h1 and the text green on /green with GET', async() => {
+    const response = await request(app)
+      .get('/green');
+
+    expect(response.text)
+      .toEqual('<h1>green</h1>');
+  });
+
+  it('responds with an h1 and the text blue on /blue with GET', async() => {
+    const response = await request(app)
+      .get('/blue');
+
+    expect(response.text)
+      .toEqual('<h1>blue</h1>');
+  });
 });
